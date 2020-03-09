00f52c2380d72c4224bbf30df956b6ba92fa0c7e
kazuki <kazuki@100you.co.jp>
2020-03-09 11:26:45 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+// 'production' or 'development'
+const MODE = "production";
+const enabledSourceMap = MODE === "production";
+
+module.exports = {
+  mode: MODE,
+  watch: true,
+  entry: {
+    index: "./src/index.js",
+    about: "./src/about.js"
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      // js
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      },
+      // sass
+      {
+        test: /\.scss$/,
+        use: [
+          // style-loader
+          { loader: "style-loader" },
+          // css-loader
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: enabledSourceMap,
+              importLoaders: 2
+            }
+          },
+          // postcss-loader  
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: enabledSourceMap,
+              plugins: [
+                require("autoprefixer")({
+                  grid: true
+                })
+              ]
+            }
+          },
+          // sass-loader
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: enabledSourceMap
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
